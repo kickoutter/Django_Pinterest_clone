d1074b3a7ed1730354f723d8a7d8345fdc7c6010
thwan97 <thwan97@gmail.com>
2021-07-14 12:09:05 +0900

    7/14 2ban class commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,5 +1,6 @@
 # 하위 분기문 작성
 # views.py에서 만든 helloworld를 보여주는 거
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreateView
@@ -9,7 +10,11 @@ app_name = 'accountapp'
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
 
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
 
-    # 가운데가 함수 들어와야할 자린데 class가 들어가서 class의 함수를 뱉어주는 .as_view() 사용
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    # 가운데가 함수 들어와야할 자린데 class가 들어가서 class의 함수를 뱉어주는 .as_view() 사용 (Class Based View 사용 위해서 사용해야댐)
     path('create/', AccountCreateView.as_view(), name='create'),
+
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -29,5 +29,6 @@ def hello_world(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:hello_world') # 성공하면 hello_world로 가라!
     template_name = 'accountapp/create.html'
+
