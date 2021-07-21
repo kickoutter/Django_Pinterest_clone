de6a202fb91c09b1cb83e29dcf9eb992454b4227
thwan97 <thwan97@gmail.com>
2021-07-21 12:41:50 +0900

    7/21 2ban class commit 4, 07. 코드단축 with decorator - 둘. @method_decorator(?,name=?) 까지

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
@@ -5,12 +6,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountCreationForm
 from accountapp.models import HelloWorld
 
 
+@login_required(login_url=reverse_lazy('accountapp:login'))
 def hello_world(request):
 
     if request.user.is_authenticated:
@@ -45,6 +48,9 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user' # 컨텍스트에서 볼 객체의 이름 설정?
     template_name = 'accountapp/detail.html'
 
+
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class AccountUpdateView(UpdateView):
     model = User # 기존 AccountCreateView와 동일한 모델 값을 사용해요.
     form_class = AccountCreationForm # form을 만들어 값을 전달하게 되요. (ID변경 막으려고, 커스터마이징한 Form을 새로 만들어 전달받아 사용.)
@@ -52,35 +58,11 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world') # 성공적으로 프로필이 완성되면 메인화면으로 넘겨줍니다.
     template_name = 'accountapp/update.html' # template경로 값을 지정해줘요.
 
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
 
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden() # 이건 사용자가 자기 계정이 아니면, 404에러가 뜨게 하는 페이지를 나타내는 것
-            # return HttpResponseRedirect(reverse('accountapp:login')) # 이건 사용자가 자기 계정이 아니면, login페이지로 가는거
-
-
-
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/delete.html'
-
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
--- decorators.py
+++ decorators.py
@@ -1,15 +1,31 @@
 
 
 def decorator(func):
-    def decorated(input_text):
+    def decorated(a, b):
         print('함수 시작!')
+
+        func(a, b)
+        print('함수 끝!\n')
+
+    def decorated(input_text):
         func(input_text)
-        print('함수 끝!')
+
     return decorated
+
+@decorator
+def rec(a, b):
+    print('사각형의 넓이 = ', a * b)
+
+@decorator
+def tri(a, b):
+    print('삼각형의 넓이 = ', a * b / 2)
 
 @decorator
 def hello_world(input_text):
     print(input_text)
 
 
-hello_world('Hello World!')
+# hello_world('Hello World!')
+
+rec(4, 4)
+tri(4, 4)
