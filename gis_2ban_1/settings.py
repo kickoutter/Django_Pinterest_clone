1942f89a89d0303b3805ac679f4fb18659da29c4
thwan97 <thwan97@gmail.com>
2021-07-22 17:24:06 +0900

    7/22 2ban class commit 2, 08.media 설정 및 Pillow 설치 (1)

--- gis_2ban_1/settings.py
+++ gis_2ban_1/settings.py
@@ -139,10 +139,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# 사진을 다루기 위한 설정 해주기
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
