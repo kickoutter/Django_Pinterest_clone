233381b6f5eadb4effd8c7095b200f04828fc819
thwan97 <thwan97@gmail.com>
2021-07-29 18:03:33 +0900

    7/29 2ban class commit 4, 13. magic grid, articleapp (2)

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from profileapp.views import ProfileCreateView, ProfileUpdateView
 
 app_name = 'articleapp'
 
 urlpatterns = [
-
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
 ]
