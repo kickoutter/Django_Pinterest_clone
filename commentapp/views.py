171d1b2c27680d64481990c4576001dda0716e68
thwan97 <thwan97@gmail.com>
2021-08-09 18:12:04 +0900

    8/9 2ban class commit 6, commentapp implementation (5), +delete.html

--- commentapp/views.py
+++ commentapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
@@ -21,5 +21,14 @@ class CommentCreateView(CreateView):
     # CreateView나 UpdateView, DeleteView를 사용하면 특정 기능을 한 뒤 어떤 페이지로 이동할 것인지 정해주는 success_url 속성이 있다.
     # 이때 url parameter를 사용한 특정 페이지로 이동할때는 get_success_url 메소드를 사용
     # 출처: https://jamanbbo.tistory.com/20 [자기계발하는 쏭이]
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
