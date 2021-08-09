a08bb2827df9be942c0a614b21a3dec71cb4167b
thwan97 <thwan97@gmail.com>
2021-08-09 18:05:08 +0900

    8/9 2ban class commit 4~5, commentapp implementation (4), +FormMixin으로 Form다중상속, (detail.html에 comment기능넣기)

--- commentapp/views.py
+++ commentapp/views.py
@@ -13,5 +13,13 @@ class CommentCreateView(CreateView):
     form_class = CommentCreationForm
     template_name = 'commentapp/create.html'
 
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        form.instance.article_id = self.request.POST.get('article_pk')
+        return super().form_valid(form)
+
+    # CreateView나 UpdateView, DeleteView를 사용하면 특정 기능을 한 뒤 어떤 페이지로 이동할 것인지 정해주는 success_url 속성이 있다.
+    # 이때 url parameter를 사용한 특정 페이지로 이동할때는 get_success_url 메소드를 사용
+    # 출처: https://jamanbbo.tistory.com/20 [자기계발하는 쏭이]
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
