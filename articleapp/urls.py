3ea59e26727927efb4def7d7f9fbfecf5f02a03e
thwan97 <thwan97@gmail.com>
2021-08-09 11:35:11 +0900

    8/4 2ban class commit 1~5, articleapp : detail, update, delete 기능

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articleapp.views import ArticleCreateView
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
 from profileapp.views import ProfileCreateView, ProfileUpdateView
 
 app_name = 'articleapp'
@@ -9,4 +9,7 @@ app_name = 'articleapp'
 urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
     path('create/', ArticleCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete'),
 ]
