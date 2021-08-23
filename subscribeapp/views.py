e0ce719e2e0b1bdfca0e793d8fc30c5f8fecc135
thwan97 <thwan97@gmail.com>
2021-08-23 17:21:00 +0900

    8/23 2ban class commit 2, Subscribe Implement > 구독 list.html

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
@@ -28,3 +29,18 @@ class SubscriptionView(RedirectView):
     # 이 작업이 끝나면 어디로 가는지
     def get_redirect_url(self, *args, **kwargs):
         return reverse('projectapp:detail', kwargs={'pk': kwargs['project_pk']})
+
+
+@method_decorator(login_required, 'get')
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+    paginate_by = 20
+
+    # get_context_data보다 좁은 범위, 템플릿의 objects들에 대해서만 접근
+    def get_queryset(self):
+        project_list = Subscription.objects.filter(user=self.request.user).values_list('project')
+        article_list = Article.objects.filter(project__in=project_list)
+        return article_list
+
