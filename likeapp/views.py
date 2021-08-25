c8a7211044615d0db8ae33848c88680b791abdf1
thwan97 <thwan97@gmail.com>
2021-08-25 12:45:11 +0900

    8/25 2ban class commit 3, Likeapp Implement > 좋아요 @transaction.atomic

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -11,25 +13,37 @@ from django.views.generic import RedirectView
 from articleapp.models import Article
 from likeapp.models import LikeRecord
 
+
+@transaction.atomic
+def db_transaction(user, article):
+    article.like += 1
+    article.save()
+
+    likeRecord = LikeRecord.objects.filter(user=user,
+                                           article=article)
+
+    if likeRecord.exists():
+        raise ValidationError('like already exists')
+    else:
+        LikeRecord(user=user, article=article).save()
+
+
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
 
     def get(self, request, *args, **kwargs):
         user = request.user
         article = Article.objects.get(pk=kwargs['article_pk'])
-        likeRecord = LikeRecord.objects.filter(user=user,
-                                               article=article)
 
-        if likeRecord.exists():
-            # 좋아요 반영 x 부분
+        try:
+            db_transaction(user, article)
+            # 좋아요 o 메세지 출력
+            messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
+        except:
+            # 좋아요 x 메세지 출력
             messages.add_message(request, messages.ERROR, '좋아요는 한번만 가능합니다.')
             return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']}))
-        else:
-            LikeRecord(user=user, article=article).save()
-            article.like += 1
-            article.save()
-            # 좋아요 반영 o 메세지 부분
-            messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
+
 
         return  super().get(request, *args, **kwargs)
 
