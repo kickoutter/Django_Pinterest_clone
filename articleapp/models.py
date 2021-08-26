96b06690e61733a284c2a47dc72ffec84253e6b4
thwan97 <thwan97@gmail.com>
2021-08-26 18:02:37 +0900

    8/26 2ban class commit 4, header.html 부분 정리

--- articleapp/models.py
+++ articleapp/models.py
@@ -10,7 +10,8 @@ class Article(models.Model):
     # ForeginKey : user가 게시물을 하나만 쓰는게 아니라 1:N인 ForeignKey를 사용. (1:1이면 OneToOne)
     # on_delete=models.SET_NULL : user가 계정삭제해도 게시물은 사라지지않음. (반대로 CASCADE는 다 사라짐)
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
-    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL,
+                                related_name='article', null=True, blank=True)
 
     title = models.CharField(max_length=200, null=True)
 
