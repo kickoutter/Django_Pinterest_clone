82862f6ef4e4053ea6c6a8061b46b059d3aae54e
thwan97 <thwan97@gmail.com>
2021-09-26 14:34:49 +0900

    9/26 (나머지따라가기) 2ban class commit 1, docker-secret

--- gis_2ban_1/settings/deploy.py
+++ gis_2ban_1/settings/deploy.py
@@ -1,24 +1,32 @@
 from .base import *
 
-env_list = dict()
+# env_list = dict()
+#
+# local_env = open(os.path.join(BASE_DIR, '.env'))
+#
+# while True:
+#     line = local_env.readline()
+#     if not line:
+#         break
+#     line = line.replace('\n', '')
+#     start = line.find('=')
+#     key = line[:start]
+#     value = line[start+1:]
+#     env_list[key] = value
 
-local_env = open(os.path.join(BASE_DIR, '.env'))
 
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n', '')
-    start = line.find('=')
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] = value
+def read_secret(secret_name):
+    with open('/run/secrets/' + secret_name) as f:
+        secret = f.read()
+        secret = secret.rstrip().lstrip()
+    return secret
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
+# SECRET_KEY = env_list['SECRET_KEY']
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -35,7 +43,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'password1234',
+        'PASSWORD': read_secret('MYSQL_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
